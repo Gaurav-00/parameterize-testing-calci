8cde1532ebaeb23190b13ae5dab313d6289945af
Gaurav-00 <shindegaurav5551@gmail.com>
2022-04-06 11:24:01 +0530

    fixtures extra

--- test_main.py
+++ test_main.py
@@ -1,37 +1,44 @@
 import main
-def test_divisible5_1():
-    a=10
-    result=main.divisible5(a)
+import pytest
+
+@pytest.fixture
+def input():
+    a=25
+    return a
+def test_divisible5_1(input):
+    #a=10
+
+    result=main.divisible5(input)
     assert result==True
-def test_divisible5_2():
-    a=11
-    result=main.divisible5(a)
+def test_divisible5_2(input):
+    #a=11
+    result=main.divisible5(input)
     assert result==False
-def test_divisible7_1():
-    a=7
-    result=main.divisible7(a)
+def test_divisible7_1(input):
+    #a=7
+    result=main.divisible7(input)
     assert result==True
 
-def test_divisible7_2():
-    a=8
-    result=main.divisible7(a)
+def test_divisible7_2(input):
+    #a=8
+    result=main.divisible7(input)
     assert result==False
 
-def test_divisible9_1():
-    a=9
-    result=main.divisible9(a)
+def test_divisible9_1(input):
+    #a=9
+    result=main.divisible9(input)
     assert result==True
-def test_divisible9_2():
-    a=11
-    result=main.divisible9(a)
+def test_divisible9_2(input):
+    #a=11
+    result=main.divisible9(input)
     assert result==False
-def test_divisible11_1():
-    a=11
-    result=main.divisible11(a)
+def test_divisible11_1(input):
+    #a=11
+    result=main.divisible11(input)
     assert result==True
-def test_divisible11_2():
-    a=12
-    result=main.divisible11(a)
+def test_divisible11_2(input):
+    #a=12
+    result=main.divisible11(input)
     assert result==False
 
 
